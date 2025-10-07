637b325b0250470f6cd27012cdd2bd070dd3faf6
Arturo <arturo167rojas@gmail.com>
2025-10-07 01:32:53 -0300

    Subiendo nuevo proyecto desde Visual Studio Code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCOW2Q-5T-bnOApwAEnuxLiydmu1epxcMY",
+    authDomain: "avicolapp.firebaseapp.com",
+    projectId: "avicolapp",
+    storageBucket: "avicolapp.firebasestorage.app",
+    messagingSenderId: "739613833371",
+    appId: "1:739613833371:web:b75b64ddf951701a5a2e3e",
+    measurementId: "G-ZT13YWYEC2"
+  }
+
+
 };
 
 /*
