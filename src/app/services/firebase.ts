70c280e795198c43cabfb909a5bf45be02745aba
Arturo <arturo167rojas@gmail.com>
2025-10-09 01:12:46 -0300

    configuracion de login con la base de datos firebase

--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -2,21 +2,33 @@ import { inject, Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { User } from '../models/user.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Firebase {
-  
+
   auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore)
 
+  // Auntenticacion
 
-// Auntenticacion
+  // Acceder
 
-// Acceder
-
-  signIn(user:User){
+  signIn(user: User) {
     return signInWithEmailAndPassword(getAuth(), user.email, user.password)
   }
 
+  // setear un documento
+
+  setDocument(path: string, data: any) {
+    return setDoc(doc(getFirestore(), path), data);
+  }
+
+
+  async getDocument(path: string) {
+    return (await getDoc(doc(getFirestore(), path))).data();
+  }
 }
